3135fc590d629ae34e511e849713c1d09a6ffacf
Navinkumar Boddu <nbod@softwareag.com>
2023-09-06 17:11:24 +0530

    Closes #2, Closes #3 Input and Json Inputs

--- src/main/java/com/practice/query/Query.java
+++ src/main/java/com/practice/query/Query.java
@@ -3,6 +3,7 @@ package com.practice.query;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.practice.request.SampleRequest;
 
 @Component
 public class Query implements GraphQLQueryResolver {
@@ -10,8 +11,16 @@ public class Query implements GraphQLQueryResolver {
 	public String firstQuery() {
 		return "FirstQuery";
 	}
-	
+
 	public String secondQuery() {
 		return "SecondQuery";
+	}
+
+	public String fullName(String firstName, String lastName) {
+		return firstName + " " + lastName;
+	}
+
+	public String fullNameUsingJSON(SampleRequest fullName) {
+		return fullName.getFirstName() + " " + fullName.getLastName();
 	}
 }
---
+++ src/main/java/com/practice/request/SampleRequest.java
@@ -0,0 +1,13 @@
+package com.practice.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SampleRequest {
+
+	private String firstName;
+
+	private String lastName;
+}
